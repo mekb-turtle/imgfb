b6cc95a9440cf45b741a32587a03f7db600068b7
Ahmad Hadwan <ahmadhadwan2004@gmail.com>
2022-05-10 19:07:02 +0300

    Removed input from stdin, image stays displayed till enter, pngs with 3 channels work

--- imgfb.c
+++ imgfb.c
@@ -8,13 +8,13 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 int usage(char* a) {
-	fprintf(stderr, "Usage: %s [<framebuffer>] <image file> [<x> <y>]\n", a);
-	fprintf(stderr, "framebuffer : framebuffer name, defaults to fb0\n");
-	fprintf(stderr, "image file : path to the farbfeld file to draw, - for stdin\n");
-	fprintf(stderr, "x y : offset of image to draw, defaults to 0 0\n");
-	return 2;
+    fprintf(stderr, "Usage: %s [<framebuffer>] <image file> [<x> <y>]\n", a);
+    fprintf(stderr, "framebuffer : framebuffer name, defaults to fb0\n");
+    fprintf(stderr, "image file : path to the farbfeld file to draw, - for stdin\n");
+    fprintf(stderr, "x y : offset of image to draw, defaults to 0 0\n");
+    return 2;
 }
-void rgba2bgra(uint8_t *pixels, int x, int y) {
+void rgba2bgra(uint8_t *pixels, int x, int y, int bpp) {
     for (int i = 0; i < y * 4; i++) {
         for (int j = 0; j < x; j+=4) {
             uint8_t b = *(pixels + j + (i * x));
@@ -24,8 +24,12 @@ void rgba2bgra(uint8_t *pixels, int x, int y) {
         }
     }
 }
-int isjpeg(char *file) { /* temporary fast way to determine if jpeg */
-    return strstr(file, ".jpg") || strstr(file, ".jpeg");
+int isfarbfeld(FILE *fp) {
+    char buff[9];
+    fread(buff, sizeof(uint8_t), 8, fp);
+    fseek(fp, 0, SEEK_SET);
+    buff[8] = 0;
+    return strncmp(buff, "farbfeld", 8) == 0;
 }
 uint8_t *decode_farbfeld(FILE *fp, int *x, int *y) {
     uint8_t *pixels, r, g, b, a;
@@ -40,114 +44,123 @@ uint8_t *decode_farbfeld(FILE *fp, int *x, int *y) {
     *x = width;
     *y = height;
 
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j+=4) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j+=4) {
 #define VAL fgetc(fp); fgetc(fp); // discard low byte
-			r = VAL;
-			g = VAL;
-			b = VAL;
-			a = VAL;
-			*(pixels + j + (i * width)) = b;
-			*(pixels + j + (i * width)) = g;
-			*(pixels + j + (i * width)) = r;
-			*(pixels + j + (i * width)) = a;
-		}
-	}
+            r = VAL;
+            g = VAL;
+            b = VAL;
+            a = VAL;
+            *(pixels + j + (i * width)) = b;
+            *(pixels + j + (i * width)) = g;
+            *(pixels + j + (i * width)) = r;
+            *(pixels + j + (i * width)) = a;
+        }
+    }
 
     return pixels;
 }
 int main(int argc, char* argv[]) {
 #define INVALID { return usage(argv[0]); }
-	if (argc < 2 || argc > 5) INVALID;
-	FILE* fb;
-	char* fb_;
-	char* size_;
-	uint32_t o_x;
-	uint32_t o_y;
-	FILE* image;
-	char* image_ = "";
-	{
-		char* fb__ = "fb0";
-		char* x_ = "0";
-		char* y_ = "0";
-		if (argc == 3 || argc == 5) { fb__ = argv[1]; }
-		if (argc == 2 || argc == 4) { image_ = argv[1]; }
-		if (argc == 3 || argc == 5) { image_ = argv[2]; }
-		if (argc == 4) { x_ = argv[2]; y_ = argv[3]; }
-		if (argc == 5) { x_ = argv[3]; y_ = argv[4]; }
-		if (strlen(x_) == 0) INVALID;
-		if (strlen(y_) == 0) INVALID;
-		if (strlen(x_) > (x_[0] == '-' ? 7 : 6)) INVALID; // too high of a number causes weirdness
-		if (strlen(y_) > (y_[0] == '-' ? 7 : 6)) INVALID;
-		if (strlen(image_) == 0) INVALID;
-		if (strlen(fb__) == 0) INVALID;
+    if (argc < 2 || argc > 5) INVALID;
+    FILE* fb;
+    char* fb_;
+    char* size_;
+    uint32_t o_x;
+    uint32_t o_y;
+    FILE* image;
+    char* image_ = "";
+    int isff;
+    uint32_t fb_w;
+    uint32_t fb_h;
+    int img_w, img_h, img_bpp;
+    uint8_t *pixels;
+    char buff;
+    {
+        char* fb__ = "fb0";
+        char* x_ = "0";
+        char* y_ = "0";
+        if (argc == 3 || argc == 5) { fb__ = argv[1]; }
+        if (argc == 2 || argc == 4) { image_ = argv[1]; }
+        if (argc == 3 || argc == 5) { image_ = argv[2]; }
+        if (argc == 4) { x_ = argv[2]; y_ = argv[3]; }
+        if (argc == 5) { x_ = argv[3]; y_ = argv[4]; }
+        if (strlen(x_) == 0) INVALID;
+        if (strlen(y_) == 0) INVALID;
+        if (strlen(x_) > (x_[0] == '-' ? 7 : 6)) INVALID; // too high of a number causes weirdness
+        if (strlen(y_) > (y_[0] == '-' ? 7 : 6)) INVALID;
+        if (strlen(image_) == 0) INVALID;
+        if (strlen(fb__) == 0) INVALID;
 #define NUMERIC(x) (x>='0' && x<='9')
 #define ALPHABETIC(x) (x>='a' && x<='z')
 #define ALPHANUMERIC(x) (ALPHABETIC(x) || NUMERIC(x))
 #define V(x) for (size_t i = 0; i < strlen(x); ++i) { if (!NUMERIC(x[i]) && !(i == 0 && x[0] == '-')) INVALID; }
-		for (size_t i = 0; i < strlen(fb__); ++i) {
-			if (!ALPHANUMERIC(fb__[i])) INVALID; }
-		V(x_); V(y_);
+        for (size_t i = 0; i < strlen(fb__); ++i) {
+            if (!ALPHANUMERIC(fb__[i])) INVALID; }
+        V(x_); V(y_);
 #undef V
-		fb_ = malloc(strlen(fb__) + 10);
-		sprintf(fb_, "/dev/%s", fb__);
-		size_ = malloc(strlen(fb__) + 50);
-		sprintf(size_, "/sys/class/graphics/%s/virtual_size", fb__);
-		o_x = atoi(x_);
-		o_y = atoi(y_);
-	}
-	uint32_t fb_w;
-	uint32_t fb_h;
+        fb_ = malloc(strlen(fb__) + 10);
+        sprintf(fb_, "/dev/%s", fb__);
+        size_ = malloc(strlen(fb__) + 50);
+        sprintf(size_, "/sys/class/graphics/%s/virtual_size", fb__);
+        o_x = atoi(x_);
+        o_y = atoi(y_);
+    }
 #define ERROR { fprintf(stderr, "%s\n", strerror(errno)); return errno; }
-	{
-		FILE* size = fopen(size_, "r");
-		if (!size) ERROR;
-		size_t i = 0;
-		uint8_t b = 0;
-		char* w_ = malloc(32);
-		char* h_ = malloc(32);
-		while (1) {
-			char c = getc(size);
-			if (NUMERIC(c)) { if (b) h_[i++] = c; else w_[i++] = c;
-			} else if (!b) { b = 1; i = 0; } else break;
-		}
-		fb_w = atoi(w_);
-		fb_h = atoi(h_);
+    {
+        FILE* size = fopen(size_, "r");
+        free(size_);
+        if (!size) ERROR;
+        size_t i = 0;
+        uint8_t b = 0;
+        char* w_ = malloc(32);
+        char* h_ = malloc(32);
+        while (1) {
+            char c = getc(size);
+            if (NUMERIC(c)) { if (b) h_[i++] = c; else w_[i++] = c;
+            } else if (!b) { b = 1; i = 0; } else break;
+        }
+        fb_w = atoi(w_);
+        fb_h = atoi(h_);
         fclose(size);
         free(w_);
         free(h_);
-	}
-	fb = fopen(fb_, "w");
-	if (!fb) ERROR;
-	if (strlen(image_) == 1 && image_[0] == '-') {
-		image = stdin;
-    } else if (isjpeg(image_)) {
-        int img_w, img_h, img_bpp;
-        unsigned char *pixels = stbi_load(image_, &img_w, &img_h, &img_bpp, STBI_rgb_alpha);
-        rgba2bgra(pixels, img_w, img_h);
-        for (uint32_t y = 0; y < img_h; y++) {
-            if (o_x>=0 && y+o_y>=0 && o_x<fb_w && y+o_y<fb_h) {
-                uint32_t j = (o_x + (y+o_y) * fb_w) * 4;
-                fseek(fb, j, SEEK_SET);
-            }
-            fwrite((pixels + (y * img_w * 4)), 1, img_w * 4, fb);
-        }
-        stbi_image_free(pixels);
+    }
+    fb = fopen(fb_, "w");
+    free(fb_);
+    if (!fb) ERROR;
+
+    image = fopen(image_, "r");
+    if (!image) ERROR;
+    
+    isff = isfarbfeld(image);
+    if (isff) {
+        pixels = decode_farbfeld(image, &img_w, &img_h);
+        if (!pixels) return 1;
     } else {
-        int img_w, img_h;
-        image = fopen(image_, "r");
-        if (!image) ERROR;
-	    unsigned char *pixels = decode_farbfeld(image, &img_w, &img_h);
-        fclose(image);
-        for (uint32_t y = 0; y < img_h; y++) {
-            if (o_x>=0 && y+o_y>=0 && o_x<fb_w && y+o_y<fb_h) {
-                uint32_t j = (o_x + (y+o_y) * fb_w) * 4;
-                fseek(fb, j, SEEK_SET);
-            }
-            fwrite((pixels + (y * img_w * 4)), 1, img_w * 4, fb);
+        pixels = stbi_load_from_file(image, &img_w, &img_h, &img_bpp, STBI_rgb_alpha);
+        if (!pixels) return 1;
+        rgba2bgra(pixels, img_w, img_h, img_bpp);
+    }
+
+    fclose(image);
+    for (uint32_t y = 0; y < img_h; y++) {
+        if (o_x>=0 && y+o_y>=0 && o_x<fb_w && y+o_y<fb_h) {
+            uint32_t j = (o_x + (y+o_y) * fb_w) * 4;
+            fseek(fb, j, SEEK_SET);
         }
-        free(pixels);
+        fwrite((pixels + (y * img_w * 4)), 1, img_w * 4, fb);
     }
     fclose(fb);
-	return 0;
+    if (isff) {
+        free(pixels);
+    } else {
+        stbi_image_free(pixels);
+    }
+
+    do {
+        read(STDIN_FILENO, &buff, 1);
+    } while (buff != '\n');
+    
+    return 0;
 }
