9d65429006bce5d54fc9974dbd54ec64ed6ae369
mekb the turtle <mekbgithub@icloud.com>
2022-05-12 10:47:28 +1000

    add jpeg support, will merge to main branch when stdin works

--- imgfb.c
+++ imgfb.c
@@ -7,160 +7,178 @@
 #include <errno.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-int usage(char* a) {
-    fprintf(stderr, "Usage: %s [<framebuffer>] <image file> [<x> <y>]\n", a);
-    fprintf(stderr, "framebuffer : framebuffer name, defaults to fb0\n");
-    fprintf(stderr, "image file : path to the farbfeld file to draw, - for stdin\n");
-    fprintf(stderr, "x y : offset of image to draw, defaults to 0 0\n");
-    return 2;
+
+int usage(char *argv0) {
+	fprintf(stderr, "Usage: %s [<framebuffer>] <image file> [<x> <y>]\n", argv0);
+	fprintf(stderr, "framebuffer : framebuffer name, defaults to fb0\n");
+	fprintf(stderr, "image file : path to the image file to draw or - for stdin, can either be farbfeld or jpeg\n");
+	fprintf(stderr, "x y : offset of image to draw, defaults to 0 0\n");
+	return 2;
 }
-void rgba2bgra(uint8_t *pixels, int x, int y, int bpp) {
-    for (int i = 0; i < y * 4; i++) {
-        for (int j = 0; j < x; j+=4) {
-            uint8_t b = *(pixels + j + (i * x));
-            uint8_t r = *(pixels + j + (i * x) + 2);
-            *(pixels + j + (i * x)) = r;
-            *(pixels + j + (i * x) + 2) = b;
-        }
-    }
+
+void rgba2bgra(uint8_t *pixels, int width, int height, int bpp) {
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			long i = (x + (y * width)) * bpp;
+			uint8_t b = pixels[i + 0];
+			uint8_t r = pixels[i + 2];
+			pixels[i + 0] = r;
+			pixels[i + 2] = b;
+		}
+	}
 }
-int isfarbfeld(FILE *fp) {
-    char buff[9];
-    fread(buff, sizeof(uint8_t), 8, fp);
-    fseek(fp, 0, SEEK_SET);
-    buff[8] = 0;
-    return strncmp(buff, "farbfeld", 8) == 0;
+bool isfarbfeld(FILE *fp) {
+	char buff[9];
+	fread(buff, 1, 8, fp);
+	fseek(fp, 0, SEEK_SET);
+	buff[8] = 0;
+	return strncmp(buff, "farbfeld", 8) == 0;
 }
-uint8_t *decode_farbfeld(FILE *fp, int *x, int *y) {
-    uint8_t *pixels, r, g, b, a;
-    int width, height;
-#define IMG(x) { if (fgetc(fp) != x) { fprintf(stderr, "Not a valid farbfeld image\n"); return NULL; }}
-    IMG('f'); IMG('a'); IMG('r'); IMG('b'); IMG('f'); IMG('e'); IMG('l'); IMG('d');
+bool isjpeg(FILE *fp) {
+	char buff[4];
+	fread(buff, 1, 3, fp);
+	fseek(fp, 0, SEEK_SET);
+	buff[3] = 0;
+	return strncmp(buff, "\xFF\xD8\xFF", 3) == 0;
+}
+uint8_t *decode_farbfeld(FILE *fp, int *width_, int *height_) {
+	fseek(fp, 8, SEEK_SET);
 #define SIZE (fgetc(fp) << 030 | fgetc(fp) << 020 | fgetc(fp) << 010 | fgetc(fp))
-    width = SIZE;
-    height = SIZE;
+	int width = SIZE;
+	int height = SIZE;
+#undef SIZE
+	uint8_t *pixels;
+	pixels = malloc(width * height * 4);
+	*width_ = width;
+	*height_ = height;
 
-    pixels = malloc(width * height * 4);
-    *x = width;
-    *y = height;
-
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j+=4) {
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
 #define VAL fgetc(fp); fgetc(fp); // discard low byte
-            r = VAL;
-            g = VAL;
-            b = VAL;
-            a = VAL;
-            *(pixels + j + (i * width)) = b;
-            *(pixels + j + (i * width)) = g;
-            *(pixels + j + (i * width)) = r;
-            *(pixels + j + (i * width)) = a;
-        }
-    }
-
-    return pixels;
+			long i = (x + (y * width)) * 4;
+			pixels[i + 2] = VAL; // R
+			pixels[i + 1] = VAL; // G
+			pixels[i + 0] = VAL; // B
+			pixels[i + 3] = VAL; // A
+#undef VAL
+		}
+	}
+	return pixels;
 }
+
 int main(int argc, char* argv[]) {
 #define INVALID { return usage(argv[0]); }
-    if (argc < 2 || argc > 5) INVALID;
-    FILE* fb;
-    char* fb_;
-    char* size_;
-    uint32_t o_x;
-    uint32_t o_y;
-    FILE* image;
-    char* image_ = "";
-    int isff;
-    uint32_t fb_w;
-    uint32_t fb_h;
-    int img_w, img_h, img_bpp;
-    uint8_t *pixels;
-    char buff;
-    {
-        char* fb__ = "fb0";
-        char* x_ = "0";
-        char* y_ = "0";
-        if (argc == 3 || argc == 5) { fb__ = argv[1]; }
-        if (argc == 2 || argc == 4) { image_ = argv[1]; }
-        if (argc == 3 || argc == 5) { image_ = argv[2]; }
-        if (argc == 4) { x_ = argv[2]; y_ = argv[3]; }
-        if (argc == 5) { x_ = argv[3]; y_ = argv[4]; }
-        if (strlen(x_) == 0) INVALID;
-        if (strlen(y_) == 0) INVALID;
-        if (strlen(x_) > (x_[0] == '-' ? 7 : 6)) INVALID; // too high of a number causes weirdness
-        if (strlen(y_) > (y_[0] == '-' ? 7 : 6)) INVALID;
-        if (strlen(image_) == 0) INVALID;
-        if (strlen(fb__) == 0) INVALID;
-#define NUMERIC(x) (x>='0' && x<='9')
-#define ALPHABETIC(x) (x>='a' && x<='z')
+	if (argc < 2 || argc > 5) INVALID;
+	char* fb_path;
+	char* size_path;
+	char* image_path;
+	int offset_x;
+	int offset_y;
+	{
+		char* fb_ = "fb0";
+		char* x_ = "0";
+		char* y_ = "0";
+		if (argc == 3 || argc == 5) { fb_ = argv[1]; }
+		if (argc == 2 || argc == 4) { image_path = argv[1]; }
+		if (argc == 3 || argc == 5) { image_path = argv[2]; }
+		if (argc == 4) { x_ = argv[2]; y_ = argv[3]; }
+		if (argc == 5) { x_ = argv[3]; y_ = argv[4]; }
+		if (strlen(x_) == 0) INVALID;
+		if (strlen(y_) == 0) INVALID;
+		if (strlen(x_) > (x_[0] == '-' ? 7 : 6)) INVALID; // too high of a number causes weirdness
+		if (strlen(y_) > (y_[0] == '-' ? 7 : 6)) INVALID;
+		if (strlen(image_path) == 0) INVALID;
+		if (strlen(fb_) == 0) INVALID;
+#define NUMERIC(x) (x>='0'&&x<='9')
+#define ALPHABETIC(x) (x>='a'&&x<='z')
 #define ALPHANUMERIC(x) (ALPHABETIC(x) || NUMERIC(x))
-#define V(x) for (size_t i = 0; i < strlen(x); ++i) { if (!NUMERIC(x[i]) && !(i == 0 && x[0] == '-')) INVALID; }
-        for (size_t i = 0; i < strlen(fb__); ++i) {
-            if (!ALPHANUMERIC(fb__[i])) INVALID; }
-        V(x_); V(y_);
-#undef V
-        fb_ = malloc(strlen(fb__) + 10);
-        sprintf(fb_, "/dev/%s", fb__);
-        size_ = malloc(strlen(fb__) + 50);
-        sprintf(size_, "/sys/class/graphics/%s/virtual_size", fb__);
-        o_x = atoi(x_);
-        o_y = atoi(y_);
-    }
+		for (size_t i = 0; i < strlen(fb_); ++i) { if (!ALPHANUMERIC(fb_[i])) INVALID; }
+		for (size_t i = 0; i < strlen(x_); ++i) { if (!NUMERIC(x_[i]) && !(i == 0 && x_[0] == '-')) INVALID; }
+		for (size_t i = 0; i < strlen(y_); ++i) { if (!NUMERIC(y_[i]) && !(i == 0 && y_[0] == '-')) INVALID; }
+		fb_path = malloc(strlen(fb_) + 10);
+		sprintf(fb_path, "/dev/%s", fb_);
+		size_path = malloc(strlen(fb_) + 50);
+		sprintf(size_path, "/sys/class/graphics/%s/virtual_size", fb_);
+		offset_x = atoi(x_);
+		offset_y = atoi(y_);
+	}
+#undef INVALID
+
 #define ERROR { fprintf(stderr, "%s\n", strerror(errno)); return errno; }
-    {
-        FILE* size = fopen(size_, "r");
-        free(size_);
-        if (!size) ERROR;
-        size_t i = 0;
-        uint8_t b = 0;
-        char* w_ = malloc(32);
-        char* h_ = malloc(32);
-        while (1) {
-            char c = getc(size);
-            if (NUMERIC(c)) { if (b) h_[i++] = c; else w_[i++] = c;
-            } else if (!b) { b = 1; i = 0; } else break;
-        }
-        fb_w = atoi(w_);
-        fb_h = atoi(h_);
-        fclose(size);
-        free(w_);
-        free(h_);
-    }
-    fb = fopen(fb_, "w");
-    free(fb_);
-    if (!fb) ERROR;
+	int fb_w;
+	int fb_h;
+	{
+		FILE* size = fopen(size_path, "r");
+		free(size_path);
+		if (!size) ERROR;
+		size_t i = 0;
+		uint8_t b = 0;
+		char* w_ = malloc(32);
+		char* h_ = malloc(32);
+		while (1) {
+			char c = getc(size);
+			if (NUMERIC(c)) {
+				if (b) { h_[i++] = c; } else { w_[i++] = c; }
+			} else {
+				if (!b) { b = 1; i = 0; } else { break; }
+			}
+		}
+		fb_w = atoi(w_);
+		fb_h = atoi(h_);
+		fclose(size);
+		free(w_);
+		free(h_);
+	}
 
-    image = fopen(image_, "r");
-    if (!image) ERROR;
-    
-    isff = isfarbfeld(image);
-    if (isff) {
-        pixels = decode_farbfeld(image, &img_w, &img_h);
-        if (!pixels) return 1;
-    } else {
-        pixels = stbi_load_from_file(image, &img_w, &img_h, &img_bpp, STBI_rgb_alpha);
-        if (!pixels) return 1;
-        rgba2bgra(pixels, img_w, img_h, img_bpp);
-    }
+	FILE* fb;
+	fb = fopen(fb_path, "w");
+	free(fb_path);
+	if (!fb) ERROR;
 
-    fclose(image);
-    for (uint32_t y = 0; y < img_h; y++) {
-        if (o_x>=0 && y+o_y>=0 && o_x<fb_w && y+o_y<fb_h) {
-            uint32_t j = (o_x + (y+o_y) * fb_w) * 4;
-            fseek(fb, j, SEEK_SET);
-        }
-        fwrite((pixels + (y * img_w * 4)), 1, img_w * 4, fb);
-    }
-    fclose(fb);
-    if (isff) {
-        free(pixels);
-    } else {
-        stbi_image_free(pixels);
-    }
+	FILE* image;
+	if (strlen(image_path) == 1 && *image_path == '-') {
+		image = stdin;
+		fprintf(stderr, "stdin currently doesn't work");
+		return;
+	} else {
+		image = fopen(image_path, "r");
+	}
+	//free(image_path); // this crashes for some reason
+	if (!image) ERROR;
 
-    do {
-        read(STDIN_FILENO, &buff, 1);
-    } while (buff != '\n');
-    
-    return 0;
+	int image_w, image_h;
+	uint8_t *pixels;
+	bool isfarbfeld_ = isfarbfeld(image);
+	bool isjpeg_ = isjpeg(image);
+	printf("%i %i",isfarbfeld_,isjpeg_);
+	if (isfarbfeld_) {
+		pixels = decode_farbfeld(image, &image_w, &image_h);
+		if (!pixels) return 1;
+	} else if (isjpeg_) {
+		int image_bpp;
+		pixels = stbi_load_from_file(image, &image_w, &image_h, &image_bpp, STBI_rgb_alpha);
+		if (!pixels) return 1;
+		rgba2bgra(pixels, image_w, image_h, image_bpp);
+	} else {
+		fprintf(stderr, "Image isn't farbfeld or jpeg\n");
+		fclose(image);
+		return 1;
+	}
+	fclose(image);
+
+	for (int y = 0; y < image_h; ++y) {
+		if (offset_x>=0 && y+offset_y>=0 && offset_x<fb_w && y+offset_y<fb_h) {
+			int j = (offset_x + (y+offset_y) * fb_w) * 4;
+			fseek(fb, j, SEEK_SET);
+		}
+		fwrite((pixels + (y * image_w * 4)), 1, image_w * 4, fb);
+	}
+	fclose(fb);
+	if (isfarbfeld_) {
+		free(pixels);
+	} else if (isjpeg_) {
+		stbi_image_free(pixels);
+	}
+
+	return 0;
 }
